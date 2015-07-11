5773c774d046b0b5945825d0fc5d283d59d5a1a2
andreajeka <andreajeka@live.com>
2015-07-11 23:48:43 +0800

    Add scoring system to the game and display them For extra feature, create a win message when certain target has been achieved

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+	/* Public vars */
 	public float speed;
+	public Text countText;
+	public Text winText;
 
+	/* Private vars */
 	private Rigidbody rb;
+	private int count;
 
+	/* Happens at the start of the game */
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText();
+		winText.text = "";
 	}
 
+	/* Update every frame */
 	void FixedUpdate() {
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
@@ -20,8 +32,19 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	}
 
+	/* Events triggered when colliding */
 	void OnTriggerEnter (Collider other) {
-		if (other.gameObject.CompareTag ("Pick Up"))
+		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count++;
+			setCountText();
+		}
+	}
+
+	void setCountText() {
+		countText.text = "Score: " + count.ToString();
+		if (count >= 12) {
+			winText.text = "YOU WIN!";
+		}
 	}
 }
